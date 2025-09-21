7a1763953072230a4f56731f0b531002fe0b77d6
IgoryanDeltoro <bondatchuk1989@gmail.com>
2025-09-21 14:29:53 +0200

    deleted extra files

--- ex02/main.d
+++
@@ -1,7 +0,0 @@
-main.o: main.cpp ShrubberyCreationForm.hpp AForm.hpp Bureaucrat.hpp \
- PresidentialPardonForm.hpp RobotomyRequestForm.hpp
-ShrubberyCreationForm.hpp:
-AForm.hpp:
-Bureaucrat.hpp:
-PresidentialPardonForm.hpp:
-RobotomyRequestForm.hpp:
